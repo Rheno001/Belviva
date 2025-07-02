27d4baf3d1e05f16f6aca389c9d08db52f5952cc
Rheno001 <renfredreuben@gmail.com>
2025-07-02 19:46:02 +0100

    update

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="text-center p-[4rem]">
+      <h1 className="text-8xl font-extrabold">404</h1>
+      <p className="text-4xl font-normal">Page not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
